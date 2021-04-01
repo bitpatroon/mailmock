ef299eafcd99719933122f00ced22df4526284bf
Bitpatroon <github@bitpatroon.nl>
2021-04-01 12:52:47 +0200

    Fixed xclass override

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,9 +7,9 @@ if (!defined('TYPO3_MODE')) {
 call_user_func(
     function () {
         // override default mailer to catch and store all e-mails int the database
-        if (class_exists('Cem\CemTesting\Core\Mail\MailMessage')) {
+        if (class_exists('Bpn\MailMock\Core\Mail\MailMessage')) {
             $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\CMS\Core\Mail\MailMessage'] =
-                ['className' => \Cem\CemTesting\Core\Mail\MailMessage::class];
+                ['className' => \Bpn\MailMock\Core\Mail\MailMessage::class];
         }
     }
 );
