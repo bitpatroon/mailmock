23c69f22b6f7eefe0c3ed7faecea439e4a1abed8
Bitpatroon <github@bitpatroon.nl>
2021-04-19 10:24:54 +0200

    Added extension dependency typo3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'BPN MailMock',
-    'description' => 'Extension catching sent e-mail for testing purposes',
-    'category' => 'misc',
-    'author' => 'Sjoerd Zonneveld',
-    'author_email' => 'typo3@bitpatroon.nl',
-    'state' => 'stable',
+    'title'          => 'BPN MailMock',
+    'description'    => 'Extension catching sent e-mail for testing purposes',
+    'category'       => 'misc',
+    'author'         => 'Sjoerd Zonneveld',
+    'author_email'   => 'typo3@bitpatroon.nl',
+    'state'          => 'stable',
     'author_company' => 'Bitpatroon',
+    'version'        => '10.4',
+    'constraints'    => [
+        'depends' => [
+            'typo3' => '10.4.0-10.9.9999',
+        ],
+    ],
 ];
